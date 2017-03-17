80a8cd9851f1f2589bc1b5be345edcbc6405c90d
yangangwang1991@gmail.com <yangangwang1991@gmail.com>
2017-03-17 23:39:30 +0800

    abc

---
+++ MvcMusicStore2/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcMusicStore2.Models
+{
+    public class Student
+    {
+        [Key]
+        public virtual int StudnetID { get; set; }
+        public virtual string Name { get; set; }
+        public virtual int Age { get; set; }
+    }
+}
---
+++ MvcMusicStore2/Models/StudentDBInitializer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcMusicStore2.Models
+{
+    public class StudentDBInitializer:System.Data.Entity.DropCreateDatabaseAlways<StudentDBContext>
+    {
+        protected override void Seed(StudentDBContext context)
+        {
+            context.Students.AddRange(new List<Student>()
+            {
+                new Student(){Name="wang"},
+                new Student(){Name="yan"},
+                new Student(){Name="yan"},
+                new Student(){Name="yan"},
+                new Student(){Name="yan"},
+                new Student(){Name="yan"},
+                new Student(){Name="gang"},
+            });
+            base.Seed(context);
+        }
+    }
+}
